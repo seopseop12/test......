c62554bc06961285bd9e8023b7f2f82e504e5c95
seopseop12 <102223236+seopseop12@users.noreply.github.com>
2022-07-28 23:18:11 +0900

    Add files via upload

---
+++ 2839.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a = int(input())
+b = 0
+
+while a >= 0:
+    if a % 5 == 0:
+        b += int(a//5)
+        print(b)
+        break
+    a -= 3
+    b += 1
+    
+else:
+    print(-1)
+
